1fa5fd66da3ef41a1b0cc4c8035d347659872c13
Freitas <freitas.adf@hotmail.com>
2018-06-04 11:07:14 +0200

    haaruu

---
+++ Kinaschack/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Andreas de Freitas && Timmy Alvelöv
+public class GameManager : MonoBehaviour
+{
+
+    #region
+
+    #endregion
+}
